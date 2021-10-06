0969b33dac21a070319907d6c9ad53c432248cb8
amaRanaweera <amaranaweera300@gmail.com>
2021-10-06 20:08:04 +0530

    person class modified

--- person.py
+++ person.py
@@ -1,9 +1,6 @@
 class Person:
-  def __init__(self, name, age):
+  name = " "
+  def __init__(self, name):
     self.name = name
-    self.age = age
 
-p1 = Person("John", 36)
 
-print(p1.name)
-print(p1.age)
