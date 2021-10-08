93f19f66877b9b3a00bdf06a822f9a1a7e8cd0c4
Amasha Ranaweera <73416084+amaRanaweera@users.noreply.github.com>
2021-10-08 14:15:29 +0530

    modify the code

--- sum.py
+++ sum.py
@@ -1,10 +1,10 @@
 number1 = input("First number: ")
 number2 = input("\nSecond number: ")
+number3 = input("\nThird number: ")  
+
+sum = float(number1) + float(number2) + float(number3)
   
 
-sum = float(number1) + float(number2)
-  
-
-print("The sum of {0} and {1} is {2}" .format(number1, number2, sum))
+print("The sum of {0} and {1} and {2} is {3}" .format(number1, number2, number3, sum))
 
 
